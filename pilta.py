a3d2da8d0f147aa251c7f0a39043c1b3362a082f
Gary345 <ivanarratia314@aragon.unam.mx>
2020-10-06 21:28:00 -0500

    Un programita en el Sr.python

---
+++ pilta.py
@@ -0,0 +1,17 @@
+pila =[3,4,5] # pila de inicio con un arreglo
+print (pila) #impresion de la pila
+#345
+pila.append(7)
+pila.append(8)
+
+print (pila)
+
+#34578
+
+print("saco este elemento",pila.pop())
+
+#8
+print("Se quedo asi la pila: ", pila)
+
+#3457
+
